a94f4bfcb04b289df7d29e8716a56b97f2df8277
John Zhang <67456865+johnzhang3410@users.noreply.github.com>
2022-04-24 03:12:47 -0400

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,19 @@
+//Swapping variables
+let a = "red";
+let b = "blue";
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
+
+
+//Returns the max of 2 numbers
+function max(a, b) {
+    return (a > b) ? a : b;
+}
+
+console.log(max(10, 15));
+
