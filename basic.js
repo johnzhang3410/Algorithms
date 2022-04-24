08448f029c1a32e1a78123f6d79ee6d62325441b
John Zhang <67456865+johnzhang3410@users.noreply.github.com>
2022-04-24 18:08:19 -0400

    Add files via upload

--- basic.js
+++ basic.js
@@ -24,3 +24,22 @@ function isLandscape(width, height) {
 }
 
 console.log(isLandscape(10, 20));
+
+
+//Divisible by 3, return Fizz
+//Divisible by 5, return Buzz
+//Divisible by both, return FizzBuzz
+//Not divisible, return number
+//Not a number passed, return not a number message
+function fizzBuzz(input) {
+    if (typeof(input) !== 'number') return console.log(NaN);
+    if ((input % 3 === 0) && (input % 5 === 0)) return console.log('FizzBuzz');
+    else if (input % 3 === 0) return console.log('Fizz');
+    else if (input % 5 === 0) return console.log('Buzz');
+    else return console.log(input);
+}
+
+fizzBuzz(true);
+
+
+
