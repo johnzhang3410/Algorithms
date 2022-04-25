e1d699d2090082566b18fa342a96da380c8b133e
John Zhang <67456865+johnzhang3410@users.noreply.github.com>
2022-04-24 22:07:11 -0400

    Add files via upload

--- basic.js
+++ basic.js
@@ -88,3 +88,92 @@ const array = ['1', '2', 3, null, undefined];
 countTruthy(array);
 
 
+//Display all string properties of an object
+function showProperties(obj) {
+    for (let string in obj) {
+        if (typeof(obj[string]) === 'string') console.log(string, obj[string]);
+    }
+}
+
+const person = {
+    name: "John",
+    year: 2002,
+    gender: 'male'
+};
+
+showProperties(person);
+
+
+//Calculates the sum of multiples of 3 and 5 below limit
+//assuming each number can only be added once
+function sum(limit) {
+    let sum = 0;
+
+    for (let i = 0; i <= limit; i++) {
+        if (i % 3 === 0) sum += i;
+        else if (i % 5 === 0) sum += i; 
+    }
+
+    console.log(sum);
+}
+
+sum(10);
+
+
+//calculates the average mark of a student and outputs a grade
+//Improvement: break this into 2 functions, one for calculating average of an array
+function calculateGrade(marks) {
+    let sum = 0;
+
+    for (let grade of marks) {
+        sum += grade;
+    }
+
+    let average = sum / marks.length;
+
+    if (average < 60) return console.log('F');
+    if (average < 70) return console.log('D');
+    if (average < 80) return console.log('C');
+    if (average < 90) return console.log('B');
+    return console.log('A');
+}
+
+let grade = [80, 10, 100];
+calculateGrade(grade);
+
+
+//We should have same # of stars as the row #
+//Defined variable pattern because we want to display it as string
+function showStars(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let pattern = '';
+        for (let j = 0; j < i; j++) {
+            pattern += '*';
+        }
+        console.log(pattern);
+    }
+}
+
+showStars(7);
+
+
+//Show all prime numbers up to the limit, first prime is 2
+//Checks each number to see if can be divided to a whole number, if yes then it's not prime
+function showPrimes(limit) {
+    for (let i = 2; i <= limit; i++) {
+
+        let isPrime = true;
+        for (let j = 2; j < i; j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime) console.log(i);
+    }
+}
+
+showPrimes(20);
+
+
